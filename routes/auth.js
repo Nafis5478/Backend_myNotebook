9a676b824b5e286cbb2bbd58283a45e2b78dcdfb
Nafis5478 <mohammad.nafis.raza.28@gmail.com>
2023-11-07 01:10:55 +0530

    auth changes

--- routes/auth.js
+++ routes/auth.js
@@ -20,21 +20,17 @@ router.post(
     body("passcode", "passcode should be of length 8").isLength({ min: 8 }),
   ],
   async (req, res) => {
-    // If there are errors, return bad request and the errors
-
+    let success = false;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
     }
-    // check whether the user with same email exist already...
     try {
-      // we will pass all the code below this in try catch form in order to avoid error...
-      let success=false;
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res
           .status(400)
-          .json({success, error: "Sorry a user with this email already exists.." });
+          .json({ success, error: "A user with this email already exists." });
       }
       const salt = await bcrypt.genSalt(10);
       const secPasscode = await bcrypt.hash(req.body.passcode, salt);
@@ -44,22 +40,17 @@ router.post(
         passcode: secPasscode,
         email: req.body.email,
       });
-      // we made the function async so no use of .then
-      // .then(user=>res.json(user))
-      // .catch(err=>{console.log(err)
-      // res.json({error:'Please enter a unique value for email'})})
       const data = {
         user: {
           id: user.id,
         },
       };
       const authToken = jwt.sign(data, process.env.JWT_SECRET);
-      success=true;
-      // res.json(user.id);
-      res.json({ success,authToken });
+      success = true;
+      res.json({ success, authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send(success,"Internal Server Error");
+      return res.status(500).send("Internal Server Error");
     }
   }
 );
